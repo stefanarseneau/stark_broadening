425285f001f0b8aa87d0875194b5ea4c701b4131
stefan <arseneausm@gmail.com>
2024-10-15 15:41:00 -0400

    built comparison code)

--- stark/catalog.py
+++ stark/catalog.py
@@ -15,6 +15,62 @@ import urllib.request
 import os
 import re
 
+def tabularize(data):
+    # Create a DataFrame directly from the JSON
+    df = pd.DataFrame([
+        {'file': key, 
+        'reference_window': values['windows'][0], 
+        'reference_rv': values['rvs'][0],
+        'reference_erv': values['e_rvs'][0],
+        'reference_teff': np.nanmean(np.array(values['teff'])),
+        'reference_logg': np.nanmean(np.array(values['logg'])),
+        'windows': np.array(values['windows']), 
+        'rvs': np.array(values['rvs']), 
+        'e_rvs': np.array(values['e_rvs']), 
+        'differentials': np.array(values['differentials']), 
+        'significance': np.array(values['significance']),
+        'redchisqr': np.array(values['redchisqr']), 
+        'teff': np.array(values['teff']), 
+        'e_teff': np.array(values['e_teff']), 
+        'logg': np.array(values['logg']), 
+        'e_logg': np.array(values['e_logg']), } 
+        for key, values in data.items()
+    ])
+    return df
+
+def apply_rules(data, sigma = 3 , rv_over_err = 3, chisqr = 10):
+    mean = np.nanmean(np.concatenate(data.rvs.values))
+    std = np.nanstd(np.concatenate(data.rvs.values))
+
+    data['mask_arr'] = data.apply(lambda row: np.logical_and(np.logical_and(
+                                np.abs(np.array(row['rvs']) / np.array(row['e_rvs'])) > rv_over_err,
+                                (np.abs(np.array(row['rvs']) - mean) <= sigma * std)),
+                                np.array(row['redchisqr'] < chisqr)), 
+                        axis=1)     
+    return data
+
+def apply_mask(data, rows = ''):
+    rows = [rows] if type(rows) != list else rows
+
+    def fill_masked_with_nan(data, value_col):
+        # Function to apply to each row
+        def apply_mask(row):
+            return np.where(row['mask_arr'], row[value_col], np.nan)
+        # Apply the function to fill masked entries
+        data[value_col] = data.apply(apply_mask, axis=1)
+        return data[value_col]
+
+    for row in rows:
+        data[row] = fill_masked_with_nan(data, row)
+    return data
+
+def bin_temperatures(data, edges):
+    temp_bn_to_range = lambda n: (edges[n-1], edges[n]) if (n != len(edges) and (n != 0)) else ((edges[n-1], np.infty) if n == len(edges) else (-np.infty, edges[n]))
+    temp_bn_to_center = lambda n: np.mean(temp_bn_to_range(n))
+
+    data['teff_bin'] = np.digitize(data['reference_teff'], edges)
+    return data, temp_bn_to_range, temp_bn_to_center
+
 def air2vac(wv):
     _tl=1.e4/np.array(wv)
     return (np.array(wv)*(1.+6.4328e-5+2.94981e-2/\
@@ -61,44 +117,3 @@ def read_spectrum(objects, dirpath, download_files = False):
         spec_table[file] = (wl, fl, ivar)
     return spec_table, objects
 
-def plot_ensemble(result_dict):
-    fig, (ax1, ax2) = plt.subplots(nrows = 2, figsize=(10,10))
-
-    for key in result_dict.keys():
-        ax1.errorbar(result_dict[key]['windows'], result_dict[key]['differentials'], 
-                        yerr = result_dict[key]['e_rvs'], markersize=8, capsize=10)
-        ax2.scatter(result_dict[key]['windows'], result_dict[key]['significance'])
-    
-    ax1.axhline(y = 0, c = 'k', ls = '--')
-    ax1.set_ylabel('$RV - RV_0$ [$km/s]$')
-
-    ax2.axhline(y = 0, c = 'k', ls = '--')
-    ax2.set_ylabel('$\\chi$ Agreement')
-    ax2.set_xlabel(r'Fit Window Size [$\AA$]')
-
-    fig, ax = plt.subplots(nrows = 1, figsize=(10,6))
-
-    rvs, wls = [], []
-    for key in result_dict.keys():
-        wls.append(result_dict[key]['windows'])
-        rvs.append(result_dict[key]['differentials'])
-    rvs = np.array(rvs, dtype=float)
-
-    wl = np.array(wls)[0,:]
-    mean_rv = np.nanmean(rvs, axis=0)
-    median_rv = np.nanmedian(rvs, axis=0)
-    std_rv = np.nanstd(rvs, axis=0)
-
-    scatter = {'s' : 40}
-    #ax.errorbar(wl, mean_rv, yerr = std_rv, label = f'Mean (n = {len(result_dict.keys())})', c = 'k', fmt = 'o', markersize=8, capsize=10)
-    ax.scatter(wl, mean_rv, label = f'Mean (n = {len(result_dict.keys())})', c = 'k', s = 60, zorder=10)
-    ax.scatter(wl, median_rv, label = f'Median (n = {len(result_dict.keys())})', c = 'blue', s = 60, zorder=10)
-
-    xmin, xmax = ax.get_xlim()
-    ax.fill_between([xmin,xmax], -1, 1, color='green', alpha=0.5, zorder=0)
-    ax.set_xlim(xmin, xmax)
-    ax.axhline(y = 0, c = 'k', ls = '--')
-    
-    ax.set_xlabel(r'Fit Window Size [$\AA$]')
-    ax.set_ylabel('$v_r - v_{r0}$ $[km /s]$')
-    ax.legend(framealpha=0)
--- stark/measure.py
+++ stark/measure.py
@@ -4,16 +4,17 @@ import numpy as np
 
 import matplotlib.pyplot as plt
 
-def test_windows(wl, fl, ivar, n = 10, plot_rvs = False):
-    centres =  dict(a = 6564.61, b = 4862.68)
-    window = dict(a = 15, b = 15,)
-    edges = dict(a = 0, b = 0)
+def test_windows(wl, fl, ivar, n = 10, lines = ['a', 'b'], plot_rvs = False):
+    centres =  dict(a = 6564.61, b = 4862.68, g = 4341.68, d = 4102.89)
+    window = dict(a = 15, b = 15, g = 15, d = 15)
+    edges = dict(a = 0, b = 0, g = 0, d = 0)
     rv_data = []
     windows = []
 
-    corvmodel = corv.models.WarwickDAModel(model_name='1d_da_nlte', names = ['a', 'b'], resolution = 0.0637, windows=window, edges=edges)
+    corvmodel = corv.models.WarwickDAModel(model_name='1d_da_nlte', names = lines, resolution = 0.0637, windows=window, edges=edges)
     ref_rv, ref_e_rv, ref_redchi, ref_param_res = corv.fit.fit_corv(wl, fl, ivar, corvmodel.model)
-    rv_data.append([ref_rv, ref_e_rv, ref_param_res])
+    rv_data.append([ref_rv, ref_e_rv, ref_param_res.redchi, ref_param_res.params['teff'].value, ref_param_res.params['teff'].stderr,
+                        ref_param_res.params['logg'].value, ref_param_res.params['logg'].stderr])
     windows.append([window['a'], window['b']])
 
     if plot_rvs:
@@ -25,26 +26,29 @@ def test_windows(wl, fl, ivar, n = 10, plot_rvs = False):
 
     window['a'] += 15
     window['b'] += 15
+    window['g'] += 15
+    window['d'] += 15
 
     steps = np.linspace(0, 70, n)
     for step in tqdm(steps):
         temp_window = window.copy()
         temp_window['a'] += step
         temp_window['b'] += step
+        temp_window['g'] += step * 0.786 # to end at 85A after n steps
+        temp_window['d'] += step * 0.643 # to end at 75A after n steps
         
-        corvmodel = corv.models.WarwickDAModel(model_name='1d_da_nlte', names = ['a', 'b'], resolution = 0.0637, windows=temp_window, edges=edges)
+        corvmodel = corv.models.WarwickDAModel(model_name='1d_da_nlte', names = lines, resolution = 0.0637, windows=temp_window, edges=edges)
         rv, e_rv, redchi, param_res = corv.fit.fit_corv(wl[~mask], fl[~mask], ivar[~mask], corvmodel.model)
         
         if plot_rvs:
             corv.utils.lineplot(wl[~mask], fl[~mask], ivar[~mask], corvmodel.model, param_res.params)
-
-        rv_data.append([rv, e_rv, param_res])
-        windows.append([temp_window['a'], temp_window['b']])
-
+        rv_data.append([rv, e_rv, param_res.redchi, param_res.params['teff'].value, param_res.params['teff'].stderr,
+                        param_res.params['logg'].value, param_res.params['logg'].stderr])
+        windows.append([temp_window['a'], temp_window['b']])    
     return np.array(rv_data), np.array(windows)
 
 def process_results(parameters, windows, plot=True, **kwargs):
-    rvs, e_rvs, params = parameters[:,0], parameters[:,1], parameters[:,1]
+    rvs, e_rvs, chisqr, teff, e_teff, logg, e_logg = parameters[:,0], parameters[:,1], parameters[:,2], parameters[:,3], parameters[:,4], parameters[:,5], parameters[:,6]
     windows = windows[:,0]
     diff = rvs - rvs[0]
     significance = lambda rv, e_rv : (rv - rvs[0]) / np.sqrt(e_rv**2 + e_rvs[0]**2)
@@ -77,7 +81,12 @@ def process_results(parameters, windows, plot=True, **kwargs):
         'rvs' : rvs,
         'e_rvs' : e_rvs,
         'differentials' : diff,
-        'significance' : sigs
+        'significance' : sigs,
+        'redchisqr' : chisqr,
+        'teff' : teff,
+        'e_teff' : e_teff,
+        'logg' : logg,
+        'e_logg' : e_logg,
     }
     return res_dict
 
@@ -91,7 +100,7 @@ def write_dict_to_json(dictionary, filename):
     with open(filename, 'w') as json_file:
         json.dump(dictionary, json_file, indent=4, default=convert_ndarray)
 
-def analyze_spectra(spec_dict, outfile, from_cache=False):
+def analyze_spectra(spec_dict, outfile, lines = ['a','b'], from_cache=False):
     if from_cache:
         with open(outfile) as json_file:
             cache = json.load(json_file)
@@ -100,13 +109,13 @@ def analyze_spectra(spec_dict, outfile, from_cache=False):
         if from_cache and (key not in list(cache.keys())):
             print(key)
             wl, fl, ivar = spec_dict[key]
-            parameters, windows = test_windows(wl, fl, ivar)
+            parameters, windows = test_windows(wl, fl, ivar, lines = lines)
             results[key] = process_results(parameters, windows, plot=False)
             write_dict_to_json(results, outfile)
         elif not from_cache:
             print(key)
             wl, fl, ivar = spec_dict[key]
-            parameters, windows = test_windows(wl, fl, ivar)
+            parameters, windows = test_windows(wl, fl, ivar, lines = lines)
             results[key] = process_results(parameters, windows, plot=False)
             write_dict_to_json(results, outfile)
     return results
